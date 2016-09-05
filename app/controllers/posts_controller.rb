f3a2a55e6bbbf5493c9070b8aa48fd6a7c26de27
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-09-05 21:39:15 +0200

    CRUD added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -13,10 +14,34 @@ class PostsController < ApplicationController
     redirect_to posts_path
   end
 
+  def show
+
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 
 end
